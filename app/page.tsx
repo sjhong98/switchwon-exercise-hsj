6f860308010951050701f243de23ea4b6fa07617
seungjaehongibrdos <162073073+seungjaehongibrdos@users.noreply.github.com>
2026-02-03 21:34:06 +0900

    feat: 최초접근로직 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
 'use client'
 
 import { useRouter } from "next/navigation";
+import { useCookies } from "react-cookie";
 
 import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
+  const [cookies] = useCookies(['accessToken']);
 
   useEffect(() => {
-    router.push('/exchange');
-  }, []);
+    const { accessToken } = cookies;
+    console.log(accessToken);
+    if (accessToken) {
+      router.push('/exchange');
+    } else {
+      router.push('/login');
+    }
+  }, [cookies]);
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-zinc-50 font-sans dark:bg-black">
